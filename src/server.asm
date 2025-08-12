380d5c744d929729e892e433ac39fd6f0ca2048e
yukittynya <lichtestv@gmail.com>
2025-08-12 11:09:20 +0200

    mov pc, laptop

--- src/server.asm
+++ src/server.asm
@@ -18,8 +18,9 @@ file_buffer:
     .space 4096
 
 .section .data
-response:
+http_200:
     .asciz "HTTP/1.0 200 OK\r\n\r\n"
+http_200_len = . - http_200 
 
 index_path:
     .asciz "index.html"
@@ -85,7 +86,6 @@ serve_connection:
 
     mov r15, rax # len of sys_read()
 
-    # parse the request!
     call parse_request
 
     mov rdi, [client_fd]
@@ -95,4 +95,94 @@ serve_connection:
     jmp accept_connection
 
 parse_request:
-    # todo parse duh >_>
+    # todo 
+    lea rdi, [buffer]
+
+    mov eax, dword ptr [rdi]
+    mov ebx, 0x20544547
+    cmp eax, ebx
+    jne send_404
+
+    add rdi, 4
+    mov rsi, rdi # start of file path, rdi will point to end
+
+    call fine_path_end
+
+fine_path_end:
+    cmp byte ptr [rdi], ' '
+    je path_found
+    inc rdi
+    cmp rdi, buffer + 1024 
+    je send_404
+    jmp fine_path_end
+
+
+path_found:
+    sub rdi, rsi
+    mov rcx, rdi
+
+    cmp rcx, 255
+    jae send_404
+
+    lea rdi, [file_path_buffer]
+    rep movsb
+    mov byte ptr [rdi], 0
+
+    lea rdi, [file_path_buffer]
+    cmp byte ptr [rdi], '/'
+    jne open_file
+    cmp byte ptr [rdi + 1], 0 
+    je send_default
+
+open_file:
+    mov rax, 2 
+    lea rdi, [file_path_buffer]
+    mov rsi, 0 
+    syscall
+
+    cmp rax, 0 
+    je send_404
+    
+    mov r14, rax # fd 
+
+    mov rax, 0 
+    mov rdi, r14 
+    lea rsi, [file_buffer]
+    mov rdx, 4095 
+    syscall
+
+    mov r13, rax # file size
+
+    mov rax, 3
+    mov rdi, r14
+    syscall 
+
+    cmp r13, 0
+    je send_404
+
+    call send_200_headers
+
+    mov rax, 1 
+    mov rdi, [client_fd]
+    lea rsi, [file_buffer]
+    mov rdx, r13 
+    syscall
+    ret
+
+send_200_headers:
+    mov rax, 1 
+    mov rdi, [client_fd]
+    lea rsi, [http_200]
+    mov rdx, http_200_len
+    syscall
+    ret
+
+send_default:
+    call send_200_headers
+
+    mov rax, 1 
+    
+    ret
+
+send_404:
+    ret
